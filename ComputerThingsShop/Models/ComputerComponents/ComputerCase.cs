065c0c3995febc2e1b9cc43af29a4e6f37c198d7
SeniorBatonio <opupep06@gmail.com>
2019-01-21 04:01:50 +0200

    Create a new base class ComputerComponent and base class ComputerCase

---
+++ ComputerThingsShop/Models/ComputerComponents/ComputerCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerThingsShop.Models.ComputerComponents
+{
+    class ComputerCase : ComputerComponent
+    {
+        string formFacktor;
+        string typeOfMotherboard;
+        string materials;
+        int numberOfFans;
+        bool powerSupply;//есть или не установлен по умолчанию
+
+        public ComputerCase(float price, string brand, string model, string formFacktor, string typeOfMotherboard,
+        string materials, int numberOfFans, bool powerSupply) : base(price, brand, model)
+        {
+            this.formFacktor = formFacktor;
+            this.typeOfMotherboard = typeOfMotherboard;
+            this.materials = materials;
+            this.numberOfFans = numberOfFans;
+            this.powerSupply = powerSupply;
+        }
+    }
+}
---
+++ ComputerThingsShop/Models/ComputerComponents/ComputerComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerThingsShop.Models.ComputerComponents
+{
+    abstract class ComputerComponent
+    {
+        private static int count = 0;
+        public static int Count
+        {
+            get => count;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Count");
+                else
+                    count = value;
+            }
+        }
+        protected float Price { get; }
+        protected string brand;
+        protected string model;
+
+        public ComputerComponent(float price, string brand, string model)
+        {
+            this.Price = price;
+            this.brand = brand;
+            this.model = model;
+            this.Price++;
+        }
+    };
+}
