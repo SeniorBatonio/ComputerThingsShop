eae5c4186520b4903302e6165c65b93cc30887c6
SeniorBatonio <opupep06@gmail.com>
2019-01-26 00:08:15 +0200

    Add class CooldownSystem

---
+++ ComputerThingsShop/Models/ComputerComponents/CooldownSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerThingsShop.Models.ComputerComponents
+{
+    class CooldownSystem : ComputerComponent
+    {
+        private	string mainFunction;
+        private string type;
+        private int diametr;
+        private int rotationSpeed;
+        private int noiseLevel;
+        private int maxTDP;
+        private string typeOfBearing;//тип 
+        public CooldownSystem(float price, string brand, string model, string mainFunction, string type, int diametr,
+        int rotationSpeed, int noiseLevel, int maxTDP, string typeOfBearing) : base(price, brand, model)
+        {
+            this.mainFunction = mainFunction;
+            this.type = type;
+            this.diametr = diametr;
+            this.rotationSpeed = rotationSpeed;
+            this.noiseLevel = noiseLevel;
+            this.maxTDP = maxTDP;
+            this.typeOfBearing = typeOfBearing;
+        }
+    }
+}
