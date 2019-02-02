391a54a96f53e875b0f1bce28630eab0f27ad70f
SeniorBatonio <opupep06@gmail.com>
2019-02-02 02:03:57 +0200

    Added class Basket

---
+++ ComputerThingsShop/Models/Basket.cs
@@ -0,0 +1,14 @@
+﻿using ComputerThingsShop.Models.ComputerComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerThingsShop.Models
+{
+    class Basket
+    {
+        List<ComputerComponent> basket = new List<ComputerComponent>();
+    }
+}
